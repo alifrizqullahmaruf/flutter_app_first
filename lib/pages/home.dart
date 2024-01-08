3ec2a4f3907712b666e84787a6731a2abb68061f
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-08 21:38:19 +0700

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// use stl shortcut
+class HomePages extends StatelessWidget {
+  const HomePages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
