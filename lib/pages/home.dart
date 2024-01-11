794681adb2159b863d0c9677ca61f85b2e51bc6c
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-11 18:08:47 +0700

    SVG-nya error sad

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 // use stl shortcut
 class HomePages extends StatelessWidget {
   const HomePages({super.key});
@@ -11,14 +12,37 @@ class HomePages extends StatelessWidget {
         title: const Text(
           'Breakfast',
           style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.white,
+        // Membuat tidak ada shadownya
         elevation: 0.0,
+        // Membuat title berada di tengah
         centerTitle: true,
+
+        // Leading = untuk bagian kiri
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          // child: SvgPicture.asset(
+          //   'assets/icons/Arrow - Left 2.svg', // Replace with the actual asset path
+          //   width: 24, // Set the width as needed
+          //   height: 24, // Set the height as needed
+          // ),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            // Add child widgets here if needed
+          ),
+        ],
       ),
     );
   }
